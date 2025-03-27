2327ac2a2484f2077b9ebbf95352df6e105493e8
simoneburrai <burrai.s98@gmail.com>
2025-03-27 15:40:54 +0100

    feat: added routes with all 5 methods, imported on app.js with router

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 const express = require("express");
 const app = express();
 const port = 4000;
+const postRouter = require("./routers/posts.js");
 
+//Setted static folder
 app.use(express.static("public"));
+
+//ROUTES
+app.get("/", (req, res) => {
+res.send("Questa è la home");
+})
+//Posts file route importing
+app.use("/posts", postRouter);
 
 
 
---
+++ routers/posts.js
@@ -0,0 +1,35 @@
+const posts = require("../data/arrayposts.js");
+const express = require("express");
+const router = express.Router();
+
+router.get("/", (req, res) => {
+	res.send(posts);
+})
+
+router.get("/:id", (req, res) => {
+	const id = parseInt(req.params.id);
+	const currentPost = posts[id-1];
+	if(id<=posts.length){
+		res.send(currentPost);
+	}else {
+		res.send(`Ci sono solo ${posts.length} post`);
+	}
+})
+
+router.post("/", (req, res) => {
+	res.send("Stiamo creando un nuovo elemento");
+})
+
+router.put("/:id", (req, res) => {
+	res.send("Stiamo modificando interamente l'elemento");	
+});
+
+router.patch("/:id", (req, res) => {
+	res.send("Stiamo modificando parzialmente l'elemento");
+});
+
+router.delete("/:id", (req, res) => {
+	res.send("Stiamo eliminando/distruggendo l'elemento");
+});
+
+module.exports = router;
